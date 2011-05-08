7cd55f2fbe229c8c7954dcd25a227eff52f7d015
Yegor Bugayenko <yegor@tpc2.com>
2011-05-08 22:08:10 +0000

    extra resources for CI

---
+++ jdbcmock/src/test/java/com/jdbcmock/JMDriverTest.java
@@ -0,0 +1,16 @@
+/**
+ * @version $Id$
+ */
+package com.jdbcmock;
+
+import org.junit.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class JMDriverTest {
+
+    @Test
+    public void testSampleOperations() throws Exception {
+    }
+
+}
